b53c2018a9fe9f79a731e17e68d2af1df4b3090d
Zaichuan You <zxy456@case.edu>
2022-12-04 18:56:34 -0500

    Plot volatility and compute volatility

--- Script.R
+++ Script.R
@@ -1,18 +1,28 @@
+options(scipen=999)
+set.seed(42)
+
+
+
 data_files <- list.files("C:/Users/21995/Desktop/Math/Stat332/Final project/STAT332/Data")
-names <-list()
+names <- c()
 for (x in 1:length(data_files)) {
   name <- data_files[x]
   if (substring(name, nchar(name)-6, nchar(name)) != "adj.csv"){
     assign(substring(name, 0, nchar(name)-4),
          read.csv(paste0("C:/Users/21995/Desktop/Math/Stat332/Final project/STAT332/Data/", data_files[x])))
-    names <- append(names, substring(name, 0, nchar(name)-4))
+    names <- c(names, substring(name, 0, nchar(name)-4))
   }
 }
 
+vola <- c()
 for (x in 1:length(names)){
-  data <- get(names[x])
-  result <- sum(data$high-data$low)
-  print(result)
+  data <- get(names[[x]])
+  result <- sum(data$high/7-data$low/7)
+  format(result, scientific = F)
+  vola <- c(vola, result)
 }
+
+print(vola)
+plot(factor(names), vola)
 
 
