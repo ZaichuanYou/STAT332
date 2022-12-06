5f8cbe5cb69cf61b194f9fb70914cef1f186492d
Zaichuan You <zxy456@case.edu>
2022-12-05 19:19:55 -0500

    Covariance plot

--- Cova.py
+++ Cova.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import os
+from sklearn.preprocessing import StandardScaler
 
 
 path = "C:/Users/21995/Desktop/量化投资/CTA/CTA/data"
@@ -18,7 +19,22 @@ for file in os.listdir():
   
         data = pd.read_csv(file_path)
         data = data.drop(columns=["time", "code", "price_adjust_factor", "bid1", "ask1", "bidvol1", "askvol1", "open", "high", "low"])
-        sns.pairplot(data, size=2.0)
-        plt.savefig(fname=f"{file[:-4]}_scattered.png")
+        stdsc = StandardScaler() 
+        X_std = stdsc.fit_transform(data)
+        cov_mat =np.cov(X_std.T)
+        plt.figure(figsize=(10,10))
+        sns.set(font_scale=1.5)
+        hm = sns.heatmap(cov_mat,
+                        cbar=True,
+                        annot=True,
+                        square=True,
+                        fmt='.2f',
+                        annot_kws={'size': 12},
+                        cmap='coolwarm',                 
+                        yticklabels=data.columns,
+                        xticklabels=data.columns)
+        plt.title('Covariance matrix showing correlation coefficients', size = 18)
+        plt.tight_layout()
+        plt.savefig(fname=f"{file[:-4]}_Covariance.png")
         #dataset[file[-3:]] = data.describe()
 
