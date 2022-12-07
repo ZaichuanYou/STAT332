3a77462d79ab53a2764a821b59805dd6c8304de4
Zaichuan You <zxy456@case.edu>
2022-12-06 20:47:42 -0500

    Vola plot

--- Cova.py
+++ Cova.py
@@ -18,23 +18,29 @@ for file in os.listdir():
         file_path = f"{path}/{file}"
   
         data = pd.read_csv(file_path)
-        data = data.drop(columns=["time", "code", "price_adjust_factor", "bid1", "ask1", "bidvol1", "askvol1", "open", "high", "low"])
-        stdsc = StandardScaler() 
-        X_std = stdsc.fit_transform(data)
-        cov_mat =np.cov(X_std.T)
-        plt.figure(figsize=(10,10))
-        sns.set(font_scale=1.5)
-        hm = sns.heatmap(cov_mat,
-                        cbar=True,
-                        annot=True,
-                        square=True,
-                        fmt='.2f',
-                        annot_kws={'size': 12},
-                        cmap='coolwarm',                 
-                        yticklabels=data.columns,
-                        xticklabels=data.columns)
-        plt.title('Covariance matrix showing correlation coefficients', size = 18)
-        plt.tight_layout()
-        plt.savefig(fname=f"{file[:-4]}_Covariance.png")
+        data["vola"] = data["high"]-data["low"]
+        # data = data.drop(columns=["time", "code", "price_adjust_factor", "bid1", "ask1", "bidvol1", "askvol1", "open", "high", "low"])
+        # stdsc = StandardScaler() 
+        # X_std = stdsc.fit_transform(data)
+        # cov_mat =np.cov(X_std.T)
+        # plt.figure(figsize=(10,10))
+        # sns.set(font_scale=1.5)
+        # hm = sns.heatmap(cov_mat,
+        #                 cbar=True,
+        #                 annot=True,
+        #                 square=True,
+        #                 fmt='.2f',
+        #                 annot_kws={'size': 12},
+        #                 cmap='coolwarm',                 
+        #                 yticklabels=data.columns,
+        #                 xticklabels=data.columns)
+
+        plt.plot(pd.to_datetime(data["time"]), data["vola"])
+        plt.xlabel("Time")
+        plt.ylabel("Volatility")
+        plt.title('Vola over time', size = 18)
+        #plt.tight_layout()
+        #plt.show()
+        plt.savefig(fname=f"{file[:-4]}_Vola.png")
         #dataset[file[-3:]] = data.describe()
 
