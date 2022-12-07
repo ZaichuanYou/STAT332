1c104fa4b56d39b03249b182e1783176131c471e
Zaichuan You <zxy456@case.edu>
2022-12-07 18:10:02 -0500

    DTW

--- Cova.py
+++ Cova.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 from sklearn.preprocessing import StandardScaler
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
 
 
 path = "C:/Users/21995/Desktop/量化投资/CTA/CTA/data"
 os.chdir(path)
   
 
-dataset = pd.DataFrame()
+dataset = {}
 # iterate through all file
 for file in os.listdir():
     # Check whether file is in text format or not
@@ -19,6 +21,7 @@ for file in os.listdir():
   
         data = pd.read_csv(file_path)
         data["vola"] = data["high"]-data["low"]
+        dataset[f"{file[:-4]}"] = data
         # data = data.drop(columns=["time", "code", "price_adjust_factor", "bid1", "ask1", "bidvol1", "askvol1", "open", "high", "low"])
         # stdsc = StandardScaler() 
         # X_std = stdsc.fit_transform(data)
@@ -35,13 +38,24 @@ for file in os.listdir():
         #                 yticklabels=data.columns,
         #                 xticklabels=data.columns)
 
-        plt.plot(pd.to_datetime(data["time"]), data["vola"])
-        plt.xlabel("Time")
-        plt.ylabel("Volatility")
-        plt.title('Vola over time', size = 18)
-        #plt.tight_layout()
-        #plt.show()
-        plt.savefig(fname="C:/Users/21995/Desktop/Math/Stat332/Final project/STAT332/Vola/"+f"{file[:-4]}_Vola.png")
-        plt.close()
-        #dataset[file[-3:]] = data.describe()
 
+
+        # plt.plot(pd.to_datetime(data["time"]), data["vola"])
+        # plt.xlabel("Time")
+        # plt.ylabel("Volatility")
+        # plt.title('Vola over time', size = 18)
+        # #plt.tight_layout()
+        # #plt.show()
+        # plt.savefig(fname="C:/Users/21995/Desktop/Math/Stat332/Final project/STAT332/Vola/"+f"{file[:-4]}_Vola.png")
+        # plt.close()
+
+dtw = np.ones((len(dataset.keys()), len(dataset.keys())))
+datalist = list(dataset.keys())
+
+for cat_1 in range(0, len(datalist)):
+    current = dataset[datalist[cat_1]]["vola"]
+    for cat_2 in range(0, len(datalist)):
+        testing = dataset[datalist[cat_2]]["vola"]
+        dtw[cat_1][cat_2] = fastdtw(current, testing, dist = euclidean)[0]
+print(datalist)
+print(dtw)
